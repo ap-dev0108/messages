1f53c8e33fa6670dd8a9b157ff735b5751e48358
Aryan <ap01n08@gmail.com>
2026-09-01 17:47:28 +0545

    chore(response): Creating a user response dto

--- messages/src/main/java/com/example/messages/dto/user/UserResponseDTO.java
+++ messages/src/main/java/com/example/messages/dto/user/UserResponseDTO.java
@@ -1,4 +1,47 @@
 package com.example.messages.dto.user;
 
+import java.time.Instant;
+import java.util.UUID;
+
 public class UserResponseDTO {
+
+    private UUID id;
+    private String username;
+    private String email;
+    private Instant createdAt;
+
+    public UserResponseDTO() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
 }
