23d9432a9ab21d717a4b5d387906fa8010a4b124
Aryan <ap01n08@gmail.com>
2026-09-01 16:00:56 +0545

    chore(interface): Creating repository

--- messages/src/main/java/com/example/messages/repository/ConversationRepository.java
+++ messages/src/main/java/com/example/messages/repository/ConversationRepository.java
@@ -1,4 +1,9 @@
 package com.example.messages.repository;
 
-public class ConversationRepository {
+import com.example.messages.entity.Conversation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ConversationRepository extends JpaRepository<Conversation, UUID> {
 }
