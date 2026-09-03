9e76636360f720fafa9e6f9a2b4e7ff70f0d94e7
Aryan <ap01n08@gmail.com>
2026-09-03 12:47:09 +0545

    refactor(repository): Adding custom query for conversation

--- messages/src/main/java/com/example/messages/repository/ConversationParticipantRepository.java
+++ messages/src/main/java/com/example/messages/repository/ConversationParticipantRepository.java
@@ -1,10 +1,27 @@
 package com.example.messages.repository;
 
+import com.example.messages.entity.Conversation;
 import com.example.messages.entity.ConversationParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ConversationParticipantRepository
         extends JpaRepository<ConversationParticipant, UUID> {
+    @Query("""
+    SELECT cp.conversation
+    FROM ConversationParticipant cp
+    WHERE cp.conversation.type = com.example.messages.entity.ConversationType.DIRECT
+      AND cp.user.id IN :userIds
+    GROUP BY cp.conversation
+    HAVING COUNT(DISTINCT cp.user.id) = 2
+""")
+    Optional<Conversation> findDirectConversationBetweenUsers(
+            @Param("userIds") List<UUID> userIds
+    );
+    List<ConversationParticipant> findByUserId(UUID userId);
 }
