c6f83787da5d26673955475cc02b376b3eac9746
Aryan <ap01n08@gmail.com>
2026-09-03 12:47:57 +0545

    feat(api): Creating post api for conversation

--- messages/src/main/java/com/example/messages/controller/ConversationController.java
+++ messages/src/main/java/com/example/messages/controller/ConversationController.java
@@ -1,4 +1,24 @@
 package com.example.messages.controller;
 
+import com.example.messages.dto.conversation.CreateConversationDTO;
+import com.example.messages.entity.Conversation;
+import com.example.messages.services.ConversationService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/conversations")
 public class ConversationController {
+    private final ConversationService conversationService;
+
+    public ConversationController(ConversationService conversationService) {
+        this.conversationService = conversationService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Conversation createConversation(@Valid @RequestBody CreateConversationDTO request) {
+        return conversationService.createConversation(request.getType(), request.getParticipantIds());
+    }
 }
