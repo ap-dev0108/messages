672aff2f5200839cf0f33dd731e990790622b953
Aryan <ap01n08@gmail.com>
2026-09-06 10:11:58 +0545

    chore(dto): Creating response dto for messages

---
+++ messages/src/main/java/com/example/messages/dto/message/MessageResponseDTO.java
@@ -0,0 +1,53 @@
+package com.example.messages.dto.message;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class MessageResponseDTO {
+
+    private UUID id;
+    private UUID conversationId;
+    private UUID senderId;
+    private String content;
+    private Instant createdAt;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(UUID conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    public UUID getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(UUID senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+}
