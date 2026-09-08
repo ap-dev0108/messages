7d5a9ab414d50bd903b3b3f1616d0f55f0847d81
Aryan <ap01n08@gmail.com>
2026-09-08 10:19:35 +0545

    mapper(message): Creating a message mapper

--- messages/src/main/java/com/example/messages/mapper/MessageMapper.java
+++ messages/src/main/java/com/example/messages/mapper/MessageMapper.java
@@ -1,4 +1,19 @@
 package com.example.messages.mapper;
 
+import com.example.messages.dto.message.MessageResponseDTO;
+import com.example.messages.entity.Message;
+
 public class MessageMapper {
+    public MessageResponseDTO toResponseDTO(Message message) {
+
+        MessageResponseDTO response = new MessageResponseDTO();
+
+        response.setId(message.getId());
+        response.setConversationId(message.getConversation().getId());
+        response.setSenderId(message.getSender().getId());
+        response.setContent(message.getContent());
+        response.setCreatedAt(message.getCreatedAt());
+
+        return response;
+    }
 }
