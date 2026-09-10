b51ea4ab23e8f0f5e5fdfd13dc6ff43f7cf5e707
Aryan <ap01n08@gmail.com>
2026-09-11 00:41:15 +0545

    feat(chat): Injecting SimpMessagingTemplate and converting the default message to it

--- messages/src/main/java/com/example/messages/controller/ChatWebSocketController.java
+++ messages/src/main/java/com/example/messages/controller/ChatWebSocketController.java
@@ -1,14 +1,28 @@
 package com.example.messages.controller;
 
+import com.example.messages.dto.message.MessageResponseDTO;
 import com.example.messages.dto.message.SendMessageDTO;
+import com.example.messages.services.MessageService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Controller
 public class ChatWebSocketController {
+    private final MessageService messageService;
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    public ChatWebSocketController(SimpMessagingTemplate simpMessagingTemplate, MessageService messageService) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+        this.messageService = messageService;
+    }
+
     @MessageMapping("/chat")
-    public void handleMessage(SendMessageDTO message) {
-        System.out.println("Conversation: " + message.getConversationId());
-        System.out.println("Messages: " + message.getContent());
+    public void handleMessage(SendMessageDTO message, Principal principal) {
+        MessageResponseDTO response = messageService.createMessageFromWebSocket(message, principal);
+
+        simpMessagingTemplate.convertAndSend("/topic/conversations/" + message.getConversationId(), response);
     }
 }
