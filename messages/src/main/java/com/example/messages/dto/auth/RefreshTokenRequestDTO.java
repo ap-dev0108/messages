59618e6048f478edfe61d727e43ee66ad959c24c
Aryan <ap01n08@gmail.com>
2026-09-22 12:47:22 +0545

    chore(dto): Creating dto for refresh token

---
+++ messages/src/main/java/com/example/messages/dto/auth/RefreshTokenRequestDTO.java
@@ -0,0 +1,16 @@
+package com.example.messages.dto.auth;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class RefreshTokenRequestDTO {
+    @NotBlank
+    private String refreshToken;
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
