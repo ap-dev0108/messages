5e26185378e2a6158f3ba61a71541d231edd3a3a
Aryan <ap01n08@gmail.com>
2026-09-22 12:54:24 +0545

    feat(api): Creating api endpoint for refresh endpoint

--- messages/src/main/java/com/example/messages/controller/AuthController.java
+++ messages/src/main/java/com/example/messages/controller/AuthController.java
@@ -1,13 +1,15 @@
 package com.example.messages.controller;
 
-import com.example.messages.dto.auth.LoginResponseDTO;
-import com.example.messages.dto.auth.UserLoginDTO;
-import com.example.messages.dto.auth.UserRegistrationDTO;
+import com.example.messages.dto.Response;
+import com.example.messages.dto.auth.*;
 import com.example.messages.dto.user.UserResponseDTO;
 import com.example.messages.services.AuthService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -37,5 +39,25 @@ public class AuthController {
         LoginResponseDTO response = authService.login(dto);
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<Response<RefreshTokenResponseDTO>> refresh(
+            @Valid @RequestBody RefreshTokenRequestDTO request
+    ) {
+
+        RefreshTokenResponseDTO response =
+                authService.refreshAccessToken(
+                        request.getRefreshToken()
+                );
+
+        return ResponseEntity.ok(
+                new Response<>(
+                        true,
+                        "Token refreshed successfully",
+                        response,
+                        Instant.now()
+                )
+        );
     }
 }
